77605e59ccb72d8388c4491f444dc5f68ff22389
Your Name <you@example.com>
2021-10-16 16:33:09 +0300

    first master commit

---
+++ Lab 5.1/Lab 5.1.cpp
@@ -0,0 +1,18 @@
+﻿// Lab 5_1
+#include <iostream>
+#include <cmath>
+using namespace std;
+double h(const double x, const double y); // прототип
+int main()
+{
+	double s,t;
+	cout << "s = "; cin >> s;
+	cout << "t = "; cin >> t;
+	double c = (h(s * s, t * t) + pow(h(s + t, 1), 2)) / (1 + pow(h(s * t, 2), 2));
+	cout << "c = " << c << endl;
+	return 0;
+}
+double h(const double x, const double y) // визначення
+{
+	return (x*y) / (1+x*x*y*y);
+}
