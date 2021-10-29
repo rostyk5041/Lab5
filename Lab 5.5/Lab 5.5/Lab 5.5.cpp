ee6aa1af910d02caee1d350e1a834be8b760d1ac
RostyslavIvato <90511382+rostyk5041@users.noreply.github.com>
2021-10-29 11:51:25 +0300

    Add files via upload

---
+++ Lab 5.5/Lab 5.5/Lab 5.5.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int f(int n)
+{
+	if (n == 1)
+		return 1;
+	if (n % 2 == 0)
+		return (2 * f(n / 2) - 1);
+	else
+		return (2 * f(n / 2) + 1);
+}
+int main()
+{
+	int n;
+	cout << "n= "; cin >> n;
+	cout <<"x=" << f(n) << endl;
+	cout << "Kp=" << n - 1 << endl;
+	return 0;
+}
